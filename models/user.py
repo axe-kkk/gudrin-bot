fc50deb9c579c7ae3cafd317a00ea113b6d136ed
Maksym Kaptsov <makskaptsov13@gmail.com>
2025-07-11 20:10:47 +0300

    update base

--- models/user.py
+++ models/user.py
@@ -5,8 +5,6 @@ from typing import Optional, TYPE_CHECKING
 from sqlmodel import Relationship
 from typing import List
 
-from models.user_interest import UserInterest
-
 if TYPE_CHECKING:
     from user import User
 
@@ -19,7 +17,6 @@ class User(SQLModel, table=True):
     diamonds: int = Field(default=0)
     referred_by: Optional[int] = Field(default=None, foreign_key="users.id")
     flag: bool = Field(default=True)
-    interests: List["UserInterest"] = Relationship(back_populates="user")
 
     referrals: List["User"] = Relationship(back_populates="referrer", sa_relationship_kwargs={"cascade": "all, delete"})
     referrer: Optional["User"] = Relationship(back_populates="referrals",
